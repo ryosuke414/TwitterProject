2ef855b326d860db22d6a32956f48d31154d4108
n_waseda <hrs2421412@stu.o-hara.ac.jp>
2025-07-01 15:03:09 +0900

    DAOだお

--- src/dao/TwitterDAO.java
+++ src/dao/TwitterDAO.java
@@ -1,5 +1,19 @@
 package dao;
 
-public class TwitterDAO {
+import java.sql.Connection;
 
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class TwitterDAO {
+	static DataSource ds;
+
+	public Connection getConnection() throws Exception {
+		if (ds==null) {
+			InitialContext ic=new InitialContext();
+			ds=(DataSource)ic.lookup("java:/comp/env/jdbc/twitter");
+		}
+		return ds.getConnection();
+
+	}
 }
